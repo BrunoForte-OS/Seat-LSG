11cab3f2fc782db2675068fd07e43a52dcd7bedc
Bruno Forte <brunoagtforte@gmail.com>
2021-01-25 19:13:41 +0000

    Ready to work

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const StylelintPlugin = require('stylelint-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { url } = require('inspector');
+const {
+    url
+} = require('inspector');
 const theme_module = '';
 
 // This is the main configuration object.
@@ -27,20 +31,18 @@ module.exports = {
     },
     watch: false,
     module: {
-        rules: [
-            {
-            test: /\.js$/,
-            /* ... */
+        rules: [{
+                test: /\.js$/,
+                /* ... */
             },
             {
-            // Apply rule for .sass, .scss or .css files
-            test: /\.(sa|sc|c)ss$/,
-        
-            // Set loaders to transform files.
-            // Loaders are applying from right to left(!)
-            // The first loader will be applied after others
-                use: [
-                    {
+                // Apply rule for .sass, .scss or .css files
+                test: /\.(sa|sc|c)ss$/,
+
+                // Set loaders to transform files.
+                // Loaders are applying from right to left(!)
+                // The first loader will be applied after others
+                use: [{
                         // After all CSS loaders we use plugin to do his work.
                         // It gets all transformed CSS and extracts it into separate
                         // single bundled file
@@ -50,52 +52,48 @@ module.exports = {
                         }
                     },
                     {
-                    // This loader resolves url() and @imports inside CSS
-                    loader: "css-loader"
+                        // This loader resolves url() and @imports inside CSS
+                        loader: "css-loader"
                     },
                     {
-                    // Then we apply postCSS fixes like autoprefixer and minifying
-                    loader: "postcss-loader"
+                        // Then we apply postCSS fixes like autoprefixer and minifying
+                        loader: "postcss-loader"
                     },
                     {
-                    // First we transform SASS to standard CSS
-                    loader: "sass-loader",
-                    options: {
-                        implementation: require("sass")
-                    }
+                        // First we transform SASS to standard CSS
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
                     }
                 ]
             },
             {
                 // Now we apply rule for images
                 test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        // Using file-loader for these files
-                        loader: "file-loader",
-        
-                        // In options we can set different things like format
-                        // and directory to save
-                        options: {
-                            name: `${theme_module}.[name].[ext]`,
-                            outputPath: `/${theme_module}/img`
-                        }
+                use: [{
+                    // Using file-loader for these files
+                    loader: "file-loader",
+
+                    // In options we can set different things like format
+                    // and directory to save
+                    options: {
+                        name: `${theme_module}.[name].[ext]`,
+                        outputPath: `/${theme_module}/img`
                     }
-                    ]
+                }]
             },
             {
                 // Apply rule for fonts files
                 test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        // Using file-loader too
-                        loader: "file-loader",
-                        options: {
-                            name: `[name].[ext]`,
-                            outputPath: `/${theme_module}`
-                        }
+                use: [{
+                    // Using file-loader too
+                    loader: "file-loader",
+                    options: {
+                        name: `[name].[ext]`,
+                        outputPath: `/${theme_module}`
                     }
-                    ]
+                }]
             }
         ]
     },
@@ -103,7 +101,7 @@ module.exports = {
 
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-          filename: "bundle.css"
+            filename: "bundle.css"
         }),
         new StylelintPlugin({
             configFile: '.stylelintrc.json',
@@ -114,19 +112,21 @@ module.exports = {
             quiet: false,
             emitErrors: true // by default this is to true to check the CSS lint errors
         }),
-        new HtmlWebpackPlugin({ template: './index.html' })
-      
-      ],
-      //devtool: 'inline-source-map',
-      devServer: {
-            contentBase: path.join(__dirname, 'dist'),
-            disableHostCheck: true,
-            hot: false,
-            https: false,
-            inline: false,
-            liveReload: true,
-            port: 3000,
-            watchContentBase: true,
-            writeToDisk: true
-      }
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+
+    ],
+    //devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        disableHostCheck: true,
+        hot: false,
+        https: false,
+        inline: false,
+        liveReload: true,
+        port: 3000,
+        watchContentBase: true,
+        writeToDisk: true
+    }
 };
